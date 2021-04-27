8b83f9e265bcfd35de8c9ca3b2e0fe626e9c2276
Jared <jaredarodriguez@gmail.com>
2021-04-27 11:29:02 -0400

    removes logs and changes scope of map

--- main.js
+++ main.js
@@ -10,20 +10,20 @@ Given an array: [10, 300, 5], print "OneZero, ThreeZeroZero,Five" into stdout.
 
 */
 
+let numMap = new Map();
+
+numMap.set('1', 'One');
+numMap.set('2', 'Two');
+numMap.set('3', 'Three');
+numMap.set('4', 'Four');
+numMap.set('5', 'Five');
+numMap.set('6', 'Six');
+numMap.set('7', 'Seven');
+numMap.set('8', 'Eight');
+numMap.set('9', 'Nine');
+numMap.set('0', 'Zero');
+
 function convertArr(arr) {
-  let numMap = new Map();
-
-  numMap.set('1', 'One');
-  numMap.set('2', 'Two');
-  numMap.set('3', 'Three');
-  numMap.set('4', 'Four');
-  numMap.set('5', 'Five');
-  numMap.set('6', 'Six');
-  numMap.set('7', 'Seven');
-  numMap.set('8', 'Eight');
-  numMap.set('9', 'Nine');
-  numMap.set('0', 'Zero');
-
   let phoneticString = '';
 
   arr.forEach((num) => {
@@ -36,5 +36,3 @@ function convertArr(arr) {
 
   return phoneticString.slice(0, -1);
 }
-
-console.log(convertArr([10, 300, 5]));
